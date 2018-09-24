b31ea7745ec8018aedde367bd2a4ae56ef9ef9fe
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-09-24 19:05:35 +0200

    Added starting tasks.

---
+++ TaskParallelLibrary/TaskParallelLibrary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskParallelLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartingTasks.CreateAndStartTask();
+
+            Thread.Sleep(3000);
+
+            StartingTasks.CreateTaskWithArg("Test text");
+
+            Thread.Sleep(3000);
+
+            StartingTasks.CalculateTextLengthInTask("Text with length equals to 29");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TaskParallelLibrary/TaskParallelLibrary/StartingTasks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    public class StartingTasks
+    {
+        public static void CreateAndStartTask()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Hello Tasks!");
+            });
+
+            var task = new Task(() => Write("?"));
+
+            task.Start();
+
+            Write('.');
+        }
+
+        public static void CreateTaskWithArg(object arg)
+        {
+            Task.Factory.StartNew(Write, arg);
+
+            var task = new Task(Write, arg);
+
+            task.Start();
+        }
+
+        public static void CalculateTextLengthInTask(object o)
+        {
+            Console.WriteLine($"Task with id {Task.CurrentId} is processing object {o}...");
+
+            var task = new Task<int>(TextLength, o);
+
+            task.Start();
+
+            Console.WriteLine($"Length of text is equals to {task.Result}");
+        }
+
+        private static int TextLength(object o)
+        {
+            return o.ToString().Length;
+        }
+
+        private static void Write(char c)
+        {
+            var i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        private static void Write(object o)
+        {
+            var i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(o.ToString());
+            }
+        }
+    }
+}
