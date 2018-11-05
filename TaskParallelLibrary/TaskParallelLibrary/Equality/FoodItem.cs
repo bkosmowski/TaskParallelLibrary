a7086b50370961a6be94ebdec4ef67f0dddbb3bf
Błażej Kosmowski <blazejkosmowski2@gmail.com>
2018-11-05 20:32:04 +0100

    Refactoring. Added implementation of equality operators. Added tests.

---
+++ TaskParallelLibrary/TaskParallelLibrary/Equality/FoodItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TaskParallelLibrary.Equality
+{
+    public enum FoodGroup { Meat, Fruit, Vegetables, Sweets }
+    
+    public struct FoodItem : IEquatable<FoodItem>
+    {
+        public FoodItem(int calories, string name, FoodGroup group)
+        {
+            Calories = calories;
+            Name = name;
+            Group = group;
+        }
+
+        public int Calories { get; }
+        public string Name { get; }
+        public FoodGroup Group { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FoodItem other) return Equals(other);
+            return false;
+        }
+
+        public bool Equals(FoodItem other)
+        {
+            return Calories == other.Calories && Name == other.Name && Group == other.Group;
+        }
+
+        public override int GetHashCode()
+        {
+            return Calories.GetHashCode() ^ Name.GetHashCode() ^ Group.GetHashCode();
+        }
+
+        public static bool operator ==(FoodItem l, FoodItem r) => l.Equals(r);
+
+        public static bool operator !=(FoodItem l, FoodItem r) => !l.Equals(r);
+    }
+}
---
+++ TaskParallelLibrary/TaskParallelLibraryTest/Equality/ValueTypeEqualityTest.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using TaskParallelLibrary.Equality;
+
+namespace TaskParallelLibraryTest.Equality
+{
+    [TestFixture]
+    public class ValueTypeEqualityTest
+    {
+        [Test]
+        public void Equals_Should_Return_True_For_FoodItems_With_Equal_Properties()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
+
+            Assert.IsTrue(apple1.Equals(apple2));
+        }
+
+        [Test]
+        public void Equals_Should_Return_False_For_FoodItems_With_Different_Properties()
+        {
+            var apple = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var banana = new FoodItem(200, "banana", FoodGroup.Fruit);
+
+            Assert.IsFalse(apple.Equals(banana));
+        }
+        
+        [Test]
+        public void GetHashCode_Should_Return_The_Same_Hashes_For_FoodItems_With_Equal_Properties()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
+
+            Assert.AreEqual(apple1.GetHashCode(), apple2.GetHashCode());
+        }
+
+        [Test]
+        public void GetHashCode_Should_Return_Different_Hashes_For_FoodItems_With_Different_Properties()
+        {
+            var apple = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var banana = new FoodItem(200, "banana", FoodGroup.Fruit);
+
+            Assert.AreNotEqual(apple.GetHashCode(), banana.GetHashCode());
+        }
+
+        [Test]
+        public void Equals_Should_Return_True_If_GetHashCode_Returns_The_Same_Hashes()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
+
+            Assert.IsTrue(apple1.Equals(apple2));
+            Assert.AreEqual(apple1.GetHashCode(), apple2.GetHashCode());
+        }
+        
+        [Test]
+        public void Equals_Should_Return_False_If_GetHashCode_Returns_Different_Hashes()
+        {
+            var apple = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var banana = new FoodItem(200, "banana", FoodGroup.Fruit);
+
+            Assert.IsFalse(apple.Equals(banana));
+            Assert.AreNotEqual(apple.GetHashCode(), banana.GetHashCode());
+        }
+
+        [Test]
+        public void Equality_Operator_Should_Return_True_For_FoodItems_With_Equal_Properties()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
+
+            Assert.IsTrue(apple1 == apple2);
+        }
+        
+        [Test]
+        public void Equality_Operator_Should_Return_False_For_FoodItems_With_Different_Properties()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
+
+            Assert.IsFalse(apple1 != apple2);
+        }
+    }
+}
