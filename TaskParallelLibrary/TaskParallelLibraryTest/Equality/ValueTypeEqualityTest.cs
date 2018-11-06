8ec32690cccb193e3c12b3fc827a920e2b04e77d
Błażej Kosmowski <blazejkosmowski2@gmail.com>
2018-11-06 19:15:28 +0100

    Added tests for inequality and usage of HashSet.

--- TaskParallelLibrary/TaskParallelLibraryTest/Equality/ValueTypeEqualityTest.cs
+++ TaskParallelLibrary/TaskParallelLibraryTest/Equality/ValueTypeEqualityTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using TaskParallelLibrary.Equality;
+using System.Collections.Generic;
 
 namespace TaskParallelLibraryTest.Equality
 {
@@ -63,6 +64,28 @@ namespace TaskParallelLibraryTest.Equality
         }
 
         [Test]
+        public void HashSet_Should_Have_Only_One_FoodItem_Despite_Inserting_Two_With_The_Same_Properties()
+        {
+            var hashSet = new HashSet<FoodItem>
+            {
+                new FoodItem(100, "apple", FoodGroup.Fruit), new FoodItem(100, "apple", FoodGroup.Fruit)
+            };
+
+            Assert.AreEqual(hashSet.Count, 1);
+        }
+
+        [Test]
+        public void HasSet_Should_Have_Two_FoodItem_When_They_Have_Different_Properties()
+        {
+            var hashSet = new HashSet<FoodItem>
+            {
+                new FoodItem(100, "apple", FoodGroup.Fruit), new FoodItem(100, "banana", FoodGroup.Fruit)
+            };
+
+            Assert.AreEqual(hashSet.Count, 2);
+        }
+
+        [Test]
         public void Equality_Operator_Should_Return_True_For_FoodItems_With_Equal_Properties()
         {
             var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
@@ -75,8 +98,26 @@ namespace TaskParallelLibraryTest.Equality
         public void Equality_Operator_Should_Return_False_For_FoodItems_With_Different_Properties()
         {
             var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var apple2 = new FoodItem(1000, "apple", FoodGroup.Fruit);
+            
+            Assert.IsFalse(apple1 == apple2);
+        }
+
+        [Test]
+        public void Inequality_Operator_Should_Return_True_For_FoodItems_With_Different_Properties()
+        {
+            var apple = new FoodItem(100, "apple", FoodGroup.Fruit);
+            var banana = new FoodItem(200, "banana", FoodGroup.Fruit);
+            
+            Assert.IsTrue(apple != banana);
+        }
+
+        [Test]
+        public void Inequality_Operator_Should_Return_False_For_FoodItems_With_Equal_Properties()
+        {
+            var apple1 = new FoodItem(100, "apple", FoodGroup.Fruit);
             var apple2 = new FoodItem(100, "apple", FoodGroup.Fruit);
-            //TODO: WTF?
+
             Assert.IsFalse(apple1 != apple2);
         }
     }
