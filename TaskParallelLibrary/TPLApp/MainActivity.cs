f218d12ae14cf1d7640038c4c2cc9e6adc5dd224
blazej.kosmowski <blazejkosmowski2@gmail.com>
2019-01-01 18:15:07 +0100

    Catch ui context

--- TaskParallelLibrary/TPLApp/MainActivity.cs
+++ TaskParallelLibrary/TPLApp/MainActivity.cs
@@ -1,18 +1,40 @@
-﻿using Android.App;
+﻿using System;
+using System.Threading;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Widget;
 
 namespace TPLApp
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
 	public class MainActivity : AppCompatActivity
 	{
+	    private Button _syncButton;
+	    private Button _asyncButton;
+	    private Button _syncContextButton;
+	    private TextView _textView;
+
+	    private SynchronizationContext _uiSyncContext;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
 			SetContentView(Resource.Layout.activity_main);
+            _uiSyncContext = SynchronizationContext.Current;
+
+		    _syncButton = FindViewById<Button>(Resource.Id.SyncButton);
+		    _asyncButton = FindViewById<Button>(Resource.Id.AsyncButton);
+		    _syncContextButton = FindViewById<Button>(Resource.Id.SyncContextButton);
+		    _textView = FindViewById<TextView>(Resource.Id.TextView);
+		    _syncContextButton.Click += OnClickSyncContextButton;
 		}
+
+	    private void OnClickSyncContextButton(object sender, EventArgs e)
+	    {
+	        _uiSyncContext.Post(_ => _textView.Text = "UI context captured", null);
+	    }
 	}
 }
 
