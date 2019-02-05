8bd0b27de5845f21b9ef4a57904c129d4bf40bb3
błażej.kosmowski <blazej.kosmowski@legimi.com>
2019-02-05 16:21:26 +0100

    Added SimpleEnumerable

---
+++ TaskParallelLibrary/TaskParallelLibrary/Enumerable/SimpleEnumerable.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskParallelLibrary.Enumerable
+{
+    public class SimpleEnumerable
+    {
+        public SimpleEnumerator GetEnumerator()
+        {
+            return new SimpleEnumerator();
+        }
+    }
+
+    public class SimpleEnumerator
+    {
+        public bool MoveNext()
+        {
+            return true;
+        }
+
+        public object Current => 50;
+    }
+
+    public class SimpleEnumerator<T>
+    {
+        public bool MoveNext()
+        {
+            return true;
+        }
+
+        public T Current { get; }
+    }
+}
