56224477c4ee6d90f09db62148ba8f35fd1fed60
błażej.kosmowski <blazej.kosmowski@legimi.com>
2019-02-05 16:21:42 +0100

    Added SimpleQuery

---
+++ TaskParallelLibrary/TaskParallelLibrary/Query/SimpleQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using TaskParallelLibrary.Enumerable;
+
+namespace TaskParallelLibrary.Query
+{
+    public class SimpleQuery<T>
+    {
+        public SimpleEnumerator<T> GetEnumerator()
+        {
+            return new SimpleEnumerator<T>();
+        }
+
+        public SimpleQuery<T> Where(Expression<Func<T, bool>> f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SimpleQuery<TR> Select<TR>(Expression<Func<T, TR>> f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Expression Expression => throw new NotImplementedException();
+    }
+}
